a96857a8e8c27ca8d30be902c421c80683c04b9d
ZandrexQX <91482671+ZandrexQX@users.noreply.github.com>
2023-06-22 07:25:02 +0300

    Edit

--- Model/Model.py
+++ Model/Model.py
@@ -1,12 +1,13 @@
-from src.Operations import *
+from Model.Input import inputData
 
-def input(a,oper,b):
-    operation = Operations(a,b)
-    if oper == "+":
-        operation = Add(a,b)
-    elif oper == "/":
-        operation = Division(a,b)
-    elif oper == "*":
-        operation = Multiplication(a,b)
-    else: return "Такой операции нет"
-    return operation.oper()
+
+class Calculator():
+    def __init__(self, a, oper, b):
+        self.__a = a
+        self.__oper = oper
+        self.__b = b
+
+    def run(self):
+        if (self.__a).isdigit() and (self.__b).isdigit():
+            return inputData(self.__a, self.__oper, self.__b)
+        else: return "Введенные данные не являются числами"
